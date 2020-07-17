cbb3bb2318e845fc8377a76b2ced5ba31f019d09
walker1983 <63851100+walker1983@users.noreply.github.com>
2020-07-17 13:45:33 +0200

    fb auth try 4

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function (req, res, next) {
   res.redirect('auth/login')
 });
 
+/* GET users listing. */
+router.get('/impressum', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET users listing. */
+router.get('/about', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+
 module.exports = router;
